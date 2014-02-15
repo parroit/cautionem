8ec0f0bf6e7898cb6a89db8c7a0eee83e87dc2ce
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-02-15 20:23:13 +0100

    skeleton app

---
+++ js/cautionem.js
@@ -0,0 +1,11 @@
+define(["jquery"],function($){
+    
+    return {
+        start: function(){
+            $("#content").append(
+                $("<h1>").html("It work!")
+            );        
+        }
+    }
+    
+});
