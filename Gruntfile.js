2af8206e522961d3524bfbfd4a236b6bb0df8407
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-02-21 21:25:36 +0100

    sauce tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,40 @@
-'use strict';
+"use strict";
 
 module.exports = function(grunt) {
+
+    var browsers = [{
+        browserName: "firefox",
+        version: "19",
+        platform: "XP"
+    }, {
+        browserName: "chrome",
+        platform: "XP"
+    }, {
+        browserName: "chrome",
+        platform: "linux"
+    }, {
+        browserName: "internet explorer",
+        platform: "WIN8",
+        version: "10"
+    }, {
+        browserName: "internet explorer",
+        platform: "VISTA",
+        version: "9"
+    }];
 
     // Project configuration.
     grunt.initConfig({
         watch: {
             test: {
-                files: ['server/**/*.js'],
-                tasks: ['mochaTest'],
+                files: ["server/**/*.js"],
+                tasks: ["mochaTest"],
                 options: {
                     spawn: true
                 }
             },
             server: {
-                files: ['server/lib/**/*.js'],
-                tasks: ['express'],
+                files: ["server/lib/**/*.js"],
+                tasks: ["express"],
                 options: {
                     spawn: false
                 }
@@ -25,30 +45,45 @@ module.exports = function(grunt) {
 
             server: {
                 options: {
-                    script: 'server/lib/server.js'
+                    script: "server/lib/server.js"
                 }
             }
         },
-
+        "saucelabs-mocha": {
+            all: {
+                options: {
+                    urls: ["http://localhost:3000/test/index.html"],
+                    tunnelTimeout: 5,
+                    build: process.env.TRAVIS_JOB_ID,
+                    concurrency: 3,
+                    browsers: browsers,
+                    testname: "mocha tests",
+                    tags: ["master"]
+                }
+            }
+        },
         mochaTest: {
             test: {
                 options: {
-                    reporter: 'spec'
+                    reporter: "spec"
                 },
-                src: ['server/test/**/*.js']
+                src: ["server/test/**/*.js"]
             }
         }
     });
 
     // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-mocha-test");
+    grunt.loadNpmTasks("grunt-express-server");
+    grunt.loadNpmTasks("grunt-saucelabs");
 
 
-    grunt.registerTask('server', ['express:server','watch:server']);
-    grunt.registerTask('test', 'mochaTest');
-    grunt.registerTask('watch-test', 'watch:test');
+    grunt.registerTask("server", ["express:server","watch:server"]);
+    grunt.registerTask("test-server", "mochaTest");
+    grunt.registerTask("test", ["mochaTest","express:server","saucelabs-mocha"]););
+    grunt.registerTask("test-client", ["express:server","saucelabs-mocha"]);
+    grunt.registerTask("watch-test", "watch:test");
 
 
 };
